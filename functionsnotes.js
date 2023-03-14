d655bff113f6f125c013ae00ae242c73e0c960d3
kiwa <kiwa@fresnou.org>
2023-03-14 10:03:00 -0700

    inital commit

---
+++ functionsnotes.js
@@ -0,0 +1,61 @@
+//Functions Notes
+
+//1. define the function
+
+// function sayHello(){
+
+// console.log('Hello');
+
+// }
+
+
+
+
+
+//define
+//
+//  function printMax(){
+//
+//  let x = 6;
+//
+//  let y = 29;
+//
+//  if (x>y){
+//
+//  console.log(`the larger number is ${x}`);
+//  } else {
+//  console.log(`the larger number is ${y}`);
+//
+//  }
+//
+//  }
+//
+//  printMax();
+
+
+
+
+// function printmax(x, y) {
+// //     if (x > y){
+// //          console.log(`the larger number is ${x}`);
+// //
+// //          } else {
+// //
+// //           console.log(`the larger number is ${y}`);
+// //     }
+// // }
+// //  printMax(16, 9);
+// //
+// //  printMax(2, 75);
+// //
+// //  printMax(2, 2);
+
+
+
+
+function sum(num1, num2){
+
+    console.log(`${num1}+${num2}=${num1+num2}`);
+}
+sum(7, 10);
+sum(27, 1);
