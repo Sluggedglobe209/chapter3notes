bc174c90daf83fdf59c839755603aa2478526b43
kiwa <kiwa@fresnou.org>
2023-03-29 10:30:05 -0700

    inital commit

---
+++ arrays2.1.js
@@ -0,0 +1,22 @@
+// function positive(n){
+// let pop = [];
+// for (let i = 1; i <= n; i++){
+// pop.push(i);
+// }
+// console.log(pop);
+// }
+//  positive(10);
+
+
+
+
+function positive(n){
+    if (n <= 0) 
+let pop = [];
+for (let i = 1; i <= n; i++){
+pop.push(i);
+}
+console.log(pop);
+}
+positive(32);
+
