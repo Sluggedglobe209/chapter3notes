6e94827ae4f589c83028384f6c81b091fae7e997
kiwa <kiwa@fresnou.org>
2023-04-11 10:29:55 -0700

    inital commit

---
+++ madlibs.js
@@ -0,0 +1,18 @@
+const readline=require (`readline-sync`);
+
+
+
+let question1 = readline.question(`enter a verb`);
+let question2 = readline.question(`enter a verb`);
+let question3 = readline.question(`enter a verb`);
+let question4 = readline.question(`enter a verb`);
+let question5 = readline.question(`enter a verb`);
+let question6 = readline.question(`enter a verb`);
+let question7 = readline.question(`enter a verb`);
+let question8 = readline.question(`enter a verb`);
+let question9 = readline.question(`enter a verb`);
+let question10 = readline.question(`enter a verb`);
+let question11 = readline.question(`enter a verb`);
+let question12 = readline.question(`enter a verb`);
+let question13 = readline.question(`enter a verb`);
+
