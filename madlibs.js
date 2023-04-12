20b498359d014340c3aa8b5ca4f2209546319df4
kiwa <kiwa@fresnou.org>
2023-04-12 10:26:41 -0700

    inital commit

--- madlibs.js
+++ madlibs.js
@@ -3,16 +3,30 @@ const readline=require (`readline-sync`);
 
 
 let question1 = readline.question(`enter a verb`);
-let question2 = readline.question(`enter a verb`);
-let question3 = readline.question(`enter a verb`);
-let question4 = readline.question(`enter a verb`);
+let question2 = readline.question(`enter a noun`);
+let question3 = readline.question(`enter a adjective`);
+let question4 = readline.question(`enter a noun`);
 let question5 = readline.question(`enter a verb`);
-let question6 = readline.question(`enter a verb`);
-let question7 = readline.question(`enter a verb`);
+let question6 = readline.question(`enter a plural noun`);
+let question7 = readline.question(`enter a adjective`);
 let question8 = readline.question(`enter a verb`);
-let question9 = readline.question(`enter a verb`);
-let question10 = readline.question(`enter a verb`);
-let question11 = readline.question(`enter a verb`);
+let question9 = readline.question(`enter a noun`);
+let question10 = readline.question(`enter a noun`);
+let question11 = readline.question(`enter a adjective`);
 let question12 = readline.question(`enter a verb`);
 let question13 = readline.question(`enter a verb`);
+let question14 = readline.question(`enter a adjective`);
+let question15 = readline.question(`enter a noun`);
+let question16 = readline.question(`enter a verb`);
+let question17 = readline.question(`enter a verb`);
+let question18 = readline.question(`enter a adjective`);
+let question19 = readline.question(`enter a noun`);
+let question20 = readline.question(`enter a verb`);
+
+let mylist =[];
+mylist.push(readline.question('enter a noun'));
+mylist.push(readline.question('enter a verb'));
+mylist.push(readline.question('enter a adjective'));
+mylist.push(readline.question('enter a plural noun'));
+
 
