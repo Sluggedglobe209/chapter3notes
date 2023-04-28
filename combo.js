e5eb9aed5c331aba84de0d5bcf333fcbf8ddbe8f
kiwa <kiwa@fresnou.org>
2023-04-28 10:30:36 -0700

    inital commit

--- combo.js
+++ combo.js
@@ -7,18 +7,33 @@ myOrder = {
 }
 
 let sandwich = readLine.question(`what type of sandwich do you want? (chicken, beef, tofu): `);
-
+myOrder ={
+    sandwich:`chicken`,
+    sandwich_cost: 5.25,
+}
+myOrder ={
+    sandwich:`beef`,
+    sandwich_cost: 6.25,
+}
+myOrder ={
+    sandwich:`tofu`,
+    sandwich_cost: 5.75,
+}
 console.log(sandwich);
 
 
-let beverage = readLine.question(`would you like a beverage?`);
-if (beverage == "yes"){
-    let bevsize = readLine.question("would you like small medium or large")
+let bevchoice = readLine.question(`would you like a beverage?`);
+if (bevchoice == "yes"){
+    let bevsize = readLine.question("would you like small(1.00$) medium(1.75$) or large(2.25$)")
 }
 
-console.log(beverage);
+console.log(myOrder)
 
-let fries = readLine.question(`would you like any fries`);
 
-console.log(fries);
+
+
+
+// let fries = readLine.question(`would you like any fries`);
+//
+// console.log(fries);
 
